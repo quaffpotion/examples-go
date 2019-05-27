d509806ccb4b22c4f2d783debc2b711234d8b4fd
quaffpotion <43005901+quaffpotion@users.noreply.github.com>
2019-05-26 20:36:39 -0500

    adds multiple-ports

---
+++ multiple-ports/multiple-ports.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+	finish := make(chan bool)
+
+	server8001 := http.NewServeMux()
+	server8001.HandleFunc("/foo", foo8001)
+	server8001.HandleFunc("/bar", bar8001)
+
+	server8002 := http.NewServeMux()
+	server8002.HandleFunc("/foo", foo8002)
+	server8002.HandleFunc("/bar", bar8002)
+
+	go func() {
+		http.ListenAndServe(":8001", server8001)
+	}()
+
+	go func() {
+		http.ListenAndServe(":8002", server8002)
+	}()
+
+	<-finish
+}
+
+func foo8001(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Listening on 8001: foo "))
+}
+
+func bar8001(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Listening on 8001: bar "))
+}
+
+func foo8002(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Listening on 8002: foo "))
+}
+
+func bar8002(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Listening on 8002: bar "))
+}
+
+//Does not work with:
+//fetch('https://localhost:8001/foo').then( r => {console.log(r)} )
